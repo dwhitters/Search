7761aa3269a7cf7e437170c11386ce121648438a
David Whitters <d.whitters@yahoo.com>
2017-07-22 01:11:07 -0400

    Added -c, -n, and -v flags. In process of adding -x flag.

--- src/search.c
+++ src/search.c
@@ -23,16 +23,38 @@
 #define FILE_OFFSET 1
 /** How long each flag string is expected to be. */
 #define FLAG_LENGTH 2
+/** The value of a flag when it is set. */
+#define SET 1
+/** The value of a flag when it is not set. */
+#define NOT_SET 0
+/** Value that is used to signify a match is found in a line. */
+#define MATCH 1
 
 /** Function prototypes. */
 void parseFlags(char ** args, int num_args);
 static int dupTag(char * tags, char tag);
-void analyzeLine(char * line, char * pattern);
+int analyzeLine(char * line, char * pattern);
 void setFlag(char flag);
+void printLine(char * line, int line_num);
 
 /** Flags */
-/** Set to one when case should be ignored. */
+/** Set to one when case should be ignored. (-i) */
 int Ignore_Case = 0;
+/** Set to one when only a count of the matching lines should be printed. (-c) */
+int Only_Count = 0;
+/**
+    Set to one when only filenames of files containing a match
+    should be listed. (-l)
+*/
+int Only_File_Names = 0;
+/** Set to one when line number of pattern match should be included. (-n)*/
+int Line_Num = 0;
+/** Set to one when returning lines NOT containing the pattern. (-v) */
+int Rev_Search = 0;
+/** Set to one when only whole word pattern matches should be printed. (-w) */
+int Whole_Word = 0;
+/** Set to one when only whole line pattern matches should be printed. (-x) */
+int Whole_Line = 0;
 
 void main(int argc, char** argv)
 {
@@ -45,6 +67,8 @@ void main(int argc, char** argv)
 
     // Check if a file was named.
     int flag_offset = (pattern[0] == '-') ? 1 : 2;
+    int line_count = 0;
+    int num_match = 0;
 
     // Parse the commandline flags.
     parseFlags(argv, argc - flag_offset);
@@ -58,10 +82,35 @@ void main(int argc, char** argv)
     */
     while(fgets(line, LINE_LENGTH, f))
     {
-        analyzeLine(line, pattern);
+        ++line_count;
+        if((MATCH == analyzeLine(line, pattern)))
+        {
+            ++num_match;
+
+            if(Rev_Search == NOT_SET)
+            {
+                printLine(line, line_count);
+            }
+        }
+        else if(Rev_Search == SET)
+        {
+            printLine(line, line_count);
+        }
     }
 
-    puts("Done!");
+    // Print the number of matches.
+    if(Only_Count == SET)
+    {
+        if(Rev_Search == NOT_SET)
+        {
+            printf("%d\n", num_match);
+        }
+        else
+        {
+            // Print the number of non-matching lines.
+            printf("%d\n", line_count - num_match);
+        }
+    }
 }
 
 /**
@@ -69,22 +118,70 @@ void main(int argc, char** argv)
     is passed in.
 
     @param line A line read from a file.
+
+    @return int Set to 1 when a match is found, otherwise 0.
 */
-void analyzeLine(char * line, char * pattern)
+int analyzeLine(char * line, char * pattern)
 {
-    if(0 == Ignore_Case)
+    int ret_val = 0;
+    if(NOT_SET == Ignore_Case)
     {
-        // Case sensitive.
-        if(strstr(line, pattern) != NULL)
+        if(Whole_Line == NOT_SET)
         {
-            printf("%s\n", line);
+            // Case sensitive.
+            if(strstr(line, pattern) != NULL)
+            {
+                ret_val = 1;
+            }
+        }
+        else
+        {
+            // Compare the entire line with the pattern.
+            if(strcmp(line, pattern) == 0)
+            {
+                ret_val = 1;
+            }
         }
     }
     else
     {
-        if(strcasestr(line, pattern) != NULL)
+        if(Whole_Line == NOT_SET)
         {
-            printf("%s\n", line);
+            if(strcasestr(line, pattern) != NULL)
+            {
+                ret_val = 1;
+            }
+        }
+        else
+        {
+            if(strcasecmp(line, pattern) == 0)
+            {
+                ret_val = 1;
+            }
+        }
+    }
+
+    return ret_val;
+}
+
+/**
+    Prints the passed in string with possible formatting depending on
+    the global flags.
+
+    @param line The string to print.
+*/
+void printLine(char * line, int line_num)
+{
+    // Lines shouldn't be printed when the Only_Count flag is set.
+    if(Only_Count == NOT_SET)
+    {
+        if(NOT_SET == Line_Num)
+        {
+            printf("%s", line);
+        }
+        else
+        {
+            printf("%d:%s", line_num, line);
         }
     }
 }
@@ -118,8 +215,26 @@ void setFlag(char flag)
 {
     switch(flag)
     {
+        case 'c':
+            Only_Count = SET;
+            break;
         case 'i':
-            Ignore_Case = 1;
+            Ignore_Case = SET;
+            break;
+        case 'l':
+            Only_File_Names = SET;
+            break;
+        case 'n':
+            Line_Num = SET;
+            break;
+        case 'v':
+            Rev_Search = SET;
+            break;
+        case 'w':
+            Whole_Word = SET;
+            break;
+        case 'x':
+            Whole_Line = SET;
             break;
         default:
             break;
